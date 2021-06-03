eed91ba9464cb8fffce73dc714e51db4b9723566
Abhisek <catchdevabi@gmail.com>
2021-06-03 16:03:56 +0530

    SGX-Native version of bfs benchmark
    The ported version of BFS from the Rodinia Benchmark Suite,
    is adapted in SGX architecture in SGX-Native mode.
    
    Implements: bfs

---
+++ bfs/Enclave/Core-Library/common_rand.cpp
@@ -0,0 +1,71 @@
+/*
+Ported from V8 benchmark suite, original copyright:
+
+// Copyright 2013 the V8 project authors. All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above
+//       copyright notice, this list of conditions and the following
+//       disclaimer in the documentation and/or other materials provided
+//       with the distribution.
+//     * Neither the name of Google Inc. nor the names of its
+//       contributors may be used to endorse or promote products derived
+//       from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#define MAXRND 0x7fffffff
+
+unsigned int _common_seed = 49734321;
+
+void common_srand(unsigned int seed) {
+    _common_seed = seed;
+}
+
+int common_rand() {
+    // Robert Jenkins' 32 bit integer hash function.
+    _common_seed = ((_common_seed + 0x7ed55d16) + (_common_seed << 12))  & 0xffffffff;
+    _common_seed = ((_common_seed ^ 0xc761c23c) ^ (_common_seed >> 19)) & 0xffffffff;
+    _common_seed = ((_common_seed + 0x165667b1) + (_common_seed << 5))   & 0xffffffff;
+    _common_seed = ((_common_seed + 0xd3a2646c) ^ (_common_seed << 9))   & 0xffffffff;
+    _common_seed = ((_common_seed + 0xfd7046c5) + (_common_seed << 3))   & 0xffffffff;
+    _common_seed = ((_common_seed ^ 0xb55a4f09) ^ (_common_seed >> 16)) & 0xffffffff;
+    return _common_seed;
+}
+
+double common_randJS() {
+	return ((double) abs(common_rand()) / (double) MAXRND);
+}
+
+
+double common_norm_rand(){
+  double R1 = common_randJS(); 
+  double R2 = common_randJS(); 
+  return 0.0f + 1.0f *cos(2*3.14*R1)*sqrt(-log(R2));
+}
+/*
+void main(){
+  unsigned int temp0 = common_rand();
+  unsigned int temp1 = common_rand();
+  unsigned int temp2 = common_rand();
+  unsigned int temp3 = common_rand();
+  printf("seeds: \n%u\n%u\n%u\n%u\n", temp0, temp1, temp2, temp3);
+  printf("js: \n%f\n", common_randJS());
+}*/
