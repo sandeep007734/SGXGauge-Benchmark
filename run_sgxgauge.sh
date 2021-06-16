3057f700bdc1022699a6fe0c691b99f31011e2c2
catchabhisek <catchdevabi@gmail.com>
2021-06-16 22:41:03 +0530

    Nbench benchmark values update

--- run_sgxgauge.sh
+++ run_sgxgauge.sh
@@ -3,6 +3,9 @@
 declare -a suite=("bfs-2" "libcatena" "openssl" "pagerank-2" "pte-hashjoin" "pte-btree" "svm")
 workload_type=$1
 
+rm -rf ~/exp_${workload_type}
+mkdir ~/exp_${workload_type}
+
 for benchmark in "${suite[@]}";
 do
     if [ "$benchmark" = "svm" ]; then
@@ -28,4 +31,6 @@ do
         sudo -H -E ./run_local.sh 4
         cd ../
     fi
+    # Copy the evaluation into a folder
+    cp -rf ${benchmark}-*/evaluation/ ~/exp_${workload_type}
 done
