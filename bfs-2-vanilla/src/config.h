d6f8637d15098de5d2090b7ef3165e3454e78d5b
catchabhisek <catchdevabi@gmail.com>
2021-06-17 04:35:38 +0530

    Updated BFS Configuration

--- bfs-2-vanilla/src/config.h
+++ bfs-2-vanilla/src/config.h
@@ -2,30 +2,30 @@
 #ifdef HIGH_
     #define MIN_NODES      20
     #define MAX_NODES      ULONG_MAX
-    #define MIN_EDGES      50
-    #define MAX_INIT_EDGES 100
+    #define MIN_EDGES      3
+    #define MAX_INIT_EDGES 10
     #define MIN_WEIGHT     1
     #define MAX_WEIGHT     10
-    #define NUM_NODES      200000
+    #define NUM_NODES      150000
     #define GRAPH_FILE_PATH "vertex_high.dat"
 
 #elif LOW_
     #define MIN_NODES      20
     #define MAX_NODES      ULONG_MAX
-    #define MIN_EDGES      20
-    #define MAX_INIT_EDGES 30
+    #define MIN_EDGES      3
+    #define MAX_INIT_EDGES 10
     #define MIN_WEIGHT     1
     #define MAX_WEIGHT     10
-    #define NUM_NODES      200000
+    #define NUM_NODES      70000
     #define GRAPH_FILE_PATH "vertex_low.dat"
 
 #else
     #define MIN_NODES      20
     #define MAX_NODES      ULONG_MAX
-    #define MIN_EDGES      30
-    #define MAX_INIT_EDGES 60
+    #define MIN_EDGES      3
+    #define MAX_INIT_EDGES 10
     #define MIN_WEIGHT     1
     #define MAX_WEIGHT     10
-    #define NUM_NODES      200000
+    #define NUM_NODES      100000
     #define GRAPH_FILE_PATH "vertex_medium.dat"
 #endif
